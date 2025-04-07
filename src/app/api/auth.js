3ec6667e6babb2e86a03b32bf273b6a459b038dc
BaronLiu <baronliu1993@gmail.com>
2025-04-07 00:29:57 -0400

    auth workflow setup

--- src/app/api/auth.js
+++ src/app/api/auth.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export async function handleRegister ({formData}) {
     if (formData) {
         try {
-            const response = await axios.post('/api/register', formData);
+            const response = await axios.post('http://localhost:8080/auth/register', formData);
             return response
         } catch (error) {
             console.error("Error during login", error)
@@ -19,7 +19,7 @@ export async function handleRegister ({formData}) {
 export async function handleLogin ({formData}) {
     if (formData) {
       try {
-        const response = await axios.post('/api/login', formData)
+        const response = await axios.post('http://localhost:8080/auth/login', formData)
         return response
       } catch (error) {
         console.log(error)
