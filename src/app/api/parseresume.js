ad4482ccd99a4b4e03cca32b809581c7875cacaf
BaronLiu <baronliu1993@gmail.com>
2025-04-17 22:05:22 -0400

    implementation of the resume section

--- src/app/api/parseresume.js
+++ src/app/api/parseresume.js
@@ -1,10 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default async function parseresume (pdfFile) {
-    try {
-        const response = await axios.post('localhost:3000/resume/get-parse/')
-        return response.data
-    } catch (error) {
-        return console.error(error)
-    }
+export default async function ParseResume(pdfFile, keywordsObj) {
+  try {
+    console.log("Received keywords:", keywordsObj);
+    const keywords_str = keywordsObj.join(", ")
+    console.log(keywords_str)
+    const formData = new FormData();
+    formData.append('file', pdfFile); 
+    formData.append('keywords', keywords_str); 
+    const response = await axios.post('http://127.0.0.1:8000/resume/parse', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -3,16 +3,23 @@
 import { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Image from "next/image";
+import File from '../../../public/file.png'
 import RAGSocket from '../components/RAGsocket';
+import ParseResume from '../api/parseresume';
 import Editor from '../components/editorresume'
 import removeSingleQuoteOrJson from '../api/fixjson';
 import getProfessorData from '../api/getprofessordata';
+import { parseAppSegmentConfig } from 'next/dist/build/segment-config/app/app-segment-config';
 //Scans current resume for all keywords and then builds new resume with latex and the format given
 
 export default function resume () {
     const searchParams = useSearchParams()
     const search = searchParams.get('url')
     const [data, setData] = useState({}); 
+    const [researchInterests, setResearchInterests] = useState({})
+    const [feedback, setFeedback] = useState("")
+    const [resumeFile, setResumeFile] = useState(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -20,7 +27,9 @@ export default function resume () {
                 const response = await getProfessorData({ url: professor_url });
                 const fixedResponse = removeSingleQuoteOrJson(response.result);
                 const professorDataObject = JSON.parse(fixedResponse);
+                console.log(professorDataObject)
                 setData(professorDataObject);
+                setResearchInterests(professorDataObject.research_interests)
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -28,12 +37,39 @@ export default function resume () {
 
         fetchData();
     }, [search]);
+
+    const handleSubmit = async () => {
+        if (!resumeFile) {
+            console.warn("No file selected!");
+            return;
+        }
+        const file = resumeFile
+        try {
+            const interests = researchInterests
+            const response = await ParseResume(file, interests);
+            console.log(response.result)
+            setFeedback(response.result);
+        } catch (error) {
+            console.error("Error fetching data", error);
+        }
+    };
+
     return (
         <>
                     <div className = "flex items-start justify-center space-x-10 p-10 select-none">
                         <div className = 'flex flex-col'>
                             
                             <div className = "mt-2">
+                                <div className = "flex flex-col justify-center items-center">
+                                    <Image src={File} alt="Placeholder" width={40} height={40}/>
+                                    <input
+                                        type="file"
+                                        accept=".pdf"
+                                        onChange={(e) => setResumeFile(e.target.files[0])}
+                                    />
+                                    <h1 className = "font-sans font-medium ">Browse Files to Upload</h1>
+                                    <p className = "font-extralight text-sm">Supports PDF, JPG, PNG</p>
+                                </div>
                                 <h1 className = "text-sm font-bold border-b-2 p-1 font-sans">
                                     PROFESSOR INFORMATION
                                 </h1>
@@ -67,14 +103,17 @@ export default function resume () {
                                         <p className = "text-xs font-sans font-medium">Feedback</p>
                                     </div>
                                     <div className = "border-gray-200 p-2 font-sans text-md h-20 w-40 rounded-md border-1">
-                                    
+                                        {feedback?.result?.candidates?.[0]?.content?.parts?.[0]?.text ?? "No feedback yet"}
                                     </div>
                                 </div>
                                 
                         <div>
-                                <button className = "mt-5 font-extralight cursor-pointer text-white bg-blue-500 rounded-sm text-xs px-2 font-sans">
-                                    Change with AI
-                                </button>
+                            <button
+                                className="mt-5 font-extralight cursor-pointer text-white bg-blue-500 rounded-sm text-xs px-2 font-sans"
+                                onClick={() => handleSubmit(resumeFile)}
+                                >
+                                Change with AI
+                            </button>
                             </div>
                         </div>
         
