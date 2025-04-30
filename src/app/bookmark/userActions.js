55d689dac6c1c123519fcc23ed118c96b21ef6c3
BaronLiu <baronliu1993@gmail.com>
2025-04-30 14:48:44 -0400

    fixing the loginactions

---
+++ src/app/bookmark/userActions.js
@@ -0,0 +1,6 @@
+
+
+export default async function userApi () {
+    const response = await fetch()
+    return response
+}
--- src/app/login/loginActions.js
+++ src/app/login/loginActions.js
@@ -10,9 +10,8 @@ export default async function loginAction(prevState, formData) {
     try {
       const response = await fetch('http://localhost:8080/auth/login', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });
 
