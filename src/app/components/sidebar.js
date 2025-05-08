79db25fac241945f91b561a76f257cd70f7f3b38
BaronLiu <baronliu1993@gmail.com>
2025-05-07 23:59:40 -0400

    sidebar implementation

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -4,17 +4,58 @@ import {
     SidebarFooter,
     SidebarGroup,
     SidebarHeader,
+    SidebarGroupContent
   } from "@/shadcomponents/ui/sidebar"
+  const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+    },
+    {
+      title: "Calendar",
+      url: "#",
+      icon: Calendar,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: Search,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+  ]
    
   export function AppSidebar() {
     return (
       <Sidebar>
-        <SidebarHeader />
         <SidebarContent>
-          <SidebarGroup />
-          <SidebarGroup />
+          <SidebarGroup>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
         </SidebarContent>
-        <SidebarFooter />
       </Sidebar>
     )
   }
