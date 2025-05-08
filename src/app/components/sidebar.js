033871ecbf1f8520ebaec3593008e567a2656e12
BaronLiu <baronliu1993@gmail.com>
2025-05-07 23:56:35 -0400

    sidebar changes

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -1,7 +1,20 @@
-export default function Sidebar () {
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+  } from "@/components/ui/sidebar"
+   
+  export function AppSidebar() {
     return (
-        <>
-            
-        </>
+      <Sidebar>
+        <SidebarHeader />
+        <SidebarContent>
+          <SidebarGroup />
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
     )
-}
+  }
