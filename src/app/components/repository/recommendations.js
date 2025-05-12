bab7d524ab81515c40b94533ed35d56e5fdb02e7
BaronLiu <baronliu1993@gmail.com>
2025-05-12 18:30:44 -0400

    styling the recommendations section

--- src/app/components/repository/recommendations.js
+++ src/app/components/repository/recommendations.js
@@ -8,6 +8,12 @@ import {
   CarouselPrevious,
 } from "@/shadcomponents/ui/carousel";
 
+import { Badge } from "@/shadcomponents/ui/badge";
+import { Button } from "@/shadcomponents/ui/button";
+
+import { Book, Bookmark } from 'lucide-react';
+
+
 export default async function Recommendations() {
   const cookieStore = await cookies();
   const user_id = cookieStore.get("user_id");
@@ -23,7 +29,7 @@ export default async function Recommendations() {
   console.log(responses.matches);
   return (
     <>
-      <div>
+      <div className = "px-12 pb-10">
         <Carousel
           opts={{
             align: "start",
@@ -32,15 +38,25 @@ export default async function Recommendations() {
         >
           <CarouselContent>
             {responses.matches.map((response, index) => (
-              <CarouselItem key={index}>
-                <div className = "shadow-sm border-1 rounded-md">
-                  <h1>{response.name}</h1>
-                  <div>
-                     {response.research_interests.map((response_interests, index_interests) => (
-                        <div key = {index_interests}>
-                            {response_interests}
-                        </div>
-                     ))}
+              <CarouselItem key={index} className = "">
+                <div className="shadow-md border-1 rounded-md p-6 w-[20rem] overflow-hidden">
+                  <div className = "border-b-1 pb-12 space-y-2">
+                    <div className = "flex justify-end">
+                        <Button className = "bg-gray-50 text-gray-500 border-1 hover:bg-gray-50 cursor-pointer">
+                            <span className = "text-xs">Save</span>
+                            <Bookmark />
+                        </Button>
+                    </div>
+                    <h2 className="text-xs font-bold">
+                        <span>{response.school} </span>
+                    </h2>
+                    <h1 className="text-xl font-semibold">{response.name}</h1>
+                    <h2> {response.faculty}</h2>
+                    <div className="grid grid-cols-2 space-y-2">
+  {response.research_interests.map((response_interests, index_interests) => (
+    <Badge key={index_interests}>{response_interests}</Badge>
+  ))}
+</div>
                   </div>
                 </div>
               </CarouselItem>
