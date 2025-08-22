310b75c63907e4c033e37008988fba51657fbda2
BaronLiu <baronliu1993@gmail.com>
2025-08-22 01:30:59 -0400

    testing functions

--- src/app/components/inbox/inboxloginbutton.js
+++ src/app/components/inbox/inboxloginbutton.js
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 
 export default function InboxLoginButton({ userId }) {
   const router = useRouter();
-  console.log(userId)
   return (
     <>
       <Button
---
+++ src/app/test/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/shadcomponents/ui/button";
+
+export default function Test() {
+  return (
+    <>
+      <a href="http://localhost:8080/auth/signin-with-google">Sign in with Google</a>
+    </>
+  );
+}
