ec3689779aa2ad976fe4c99fd7c6f042303c994a
BaronLiu <baronliu1993@gmail.com>
2025-09-08 01:19:46 -0400

    error handling

--- src/app/actions/syncSnippetData.js
+++ src/app/actions/syncSnippetData.js
@@ -5,26 +5,29 @@ export const SyncSnippetData = async (
   variableArray,
   access
 ) => {
-  const response = await fetch(
-    `http://localhost:8080/snippets/sync-fetchable-variables`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${access}`
-      },
-      body: JSON.stringify({
-        professorIdArray,
-        variableArray,
-      }),
+  try {
+    const response = await fetch(
+      `http://localhost:8080/snippets/sync-fetchable-variables`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access}`,
+        },
+        body: JSON.stringify({
+          professorIdArray,
+          variableArray,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      return;
     }
-  );
 
-  if (!response.ok) {
-    console.error("Failed to fetch snippet");
-    return;
+    const data = await response.json();
+    return data;
+  } catch {
+    throw new Error("Failed")
   }
-
-  const data = await response.json();
-  return data;
 };
