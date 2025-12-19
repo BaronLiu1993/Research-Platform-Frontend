429f7e6129e831666d6938547a91ef446fa47459
BaronLiu <baronliu1993@gmail.com>
2025-12-19 17:42:50 -0500

    added send icon import

--- src/app/register/registerClientWrapper.js
+++ src/app/register/registerClientWrapper.js
@@ -7,7 +7,7 @@ import DropdownYear from "../components/dropdowns/dropdownyear";
 import DropdownMajor from "../components/dropdowns/dropdownmajor";
 import DropdownInterests from "../components/dropdowns/dropdowninterests";
 
-import { AlertCircle, Loader2 } from "lucide-react";
+import { AlertCircle, Loader2, Send } from "lucide-react";
 
 export default function RegisterClientWrapper({ access }) {
   const router = useRouter();
